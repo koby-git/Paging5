fa461fa481a5eee7742dacb8cafd6ba926974148
koby25292 <koby25292@gmail.com>
2021-08-26 19:26:23 +0300

    theme day/night

--- app/src/main/java/com/vikas/paging3/MainActivity.kt
+++ app/src/main/java/com/vikas/paging3/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vikas.paging3
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -12,5 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+
+        setUpView()
+    }
+
+    private fun setUpView() {
+        findViewById<BottomNavigationView>(R.id.navBottomBar).apply {
+            setupWithNavController(findNavController(R.id.fragmentNavHost))
+        }
     }
 }
